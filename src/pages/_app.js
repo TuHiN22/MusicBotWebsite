dce195187305e7d79a245af947f73cbcb8ea0ccc
Ziji <Zijipia@gmail.com>
2024-10-22 01:19:25 +0700

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,17 @@
 import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
 	return (
 		<SessionProvider session={session}>
+			<Head>
+				<title>Zi Dashboard</title>
+				<link
+					rel='icon'
+					href='/favicon.png'
+				/>
+			</Head>
 			<Component {...pageProps} />
 		</SessionProvider>
 	);
